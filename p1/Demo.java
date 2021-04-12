c5ff86918a622d4dcbb4de0fa3e0e9d60a4f5a0f
utsav <75293077+up1512001@users.noreply.github.com>
2021-04-12 10:59:02 +0530

    Access Specifier Usage

---
+++ p1/Demo.java
@@ -0,0 +1,10 @@
+package p1;
+// Access Specifier Usage
+public class Demo {
+    public static void main(String[] args){
+        new Protection();
+        new Derived();
+        new SamePackage();
+
+    }
+}
